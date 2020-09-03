250b8572e71e725adabd66a30e778f33d92bf8bb
Julia Lisec <julialisec12@gmail.com>
2020-09-03 05:30:54 +0200

    get random computer choice function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const userScore = 0;
 const computerScore = 0;
 
-//
+// caching the dom
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
@@ -9,3 +9,27 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const randomNumber = (Math.floor(Math.random() * 3));
+  return choices[randomNumber];
+}
+
+function game(userChoice) {
+
+}
+
+function main() {
+  rock_div.addEventListener('click', function() {
+    game("r");
+  })
+  paper_div.addEventListener('click', function() {
+    game("p");
+  })
+  scissors_div.addEventListener('click', function() {
+    game("s");
+  })
+}
+
+main();
